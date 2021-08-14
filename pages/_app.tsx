ab48f6d1d9075c6ea757383907367c817b2278c1
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-08-14 17:48:54 +0200

    Add components and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
+import { AppProps } from 'next/app'
+import Router from 'next/router'
 import React from 'react'
+import NProgress from 'nprogress'
+
 import { Page } from '../components/Page'
+import '../components/styles/nprogress.css'
 
-interface IAppProps {
-    Component: React.ComponentType
-    pageProps: React.ComponentProps<any>
-}
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
-export default function App({ Component, pageProps }: IAppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <Page>
             <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,22 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet()
+
+        const page = ctx.renderPage((App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        )
+
+        const styleTags = sheet.getStyleElement()
+        return { ...page, styleTags }
+    }
+
     render () {
         return (
             <Html lang="en-US">
-                {/* <Head></Head> */}
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
