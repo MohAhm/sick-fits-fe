74aee3cf79946533f3fd2bfe51b16b068665c378
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-09-19 21:25:49 +0200

    Querying the current user

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
+import { useUser } from "./User";
 
 interface INavProps {}
 
 export const Nav: React.FC<INavProps> = () => {
+  const user = useUser();
+  console.log("User", user);
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && <Link href="/signin">Sign In</Link>}
     </NavStyles>
   );
 };
